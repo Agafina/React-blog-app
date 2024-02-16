ec77531ed94a86ecb0a3d600661f109cd0ea57fe
Agafina <98034773+Agafina@users.noreply.github.com>
2024-02-16 17:01:20 +0100

    added a Delete button to delete the post

--- src/DetailsBlog.js
+++ src/DetailsBlog.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const DetailsBlog = () => {
     const { id } = useParams();
     const { data: blogs, error, isPending} = useFetch(`http://localhost:8000/blogs/` + id) 
+    const history = useHistory()
+
+    const handleDelete = () =>{
+        fetch(`http://localhost:8000/blogs/` + id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/')
+        })
+    }
     return ( 
         <div className="blog-details">
             {isPending && <div>Loading ...</div>}
@@ -13,7 +22,9 @@ const DetailsBlog = () => {
                     <h2>{ blogs.title}</h2>
                     <p>{ blogs.author}</p>
                     <div className="blog-body"> { blogs.body }</div>
+                    <button onClick={handleDelete}>delete</button>
                 </article>
+                
             )}
         </div>
      );
